8198d34652fe9114c044d0c6ecde4a03f46258b3
pashaapsky <ap.sky@yandex.ru>
2020-08-31 21:07:11 +0300

    add second symfony console command

--- app.php
+++ app.php
@@ -3,10 +3,12 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use App\Command\printStrCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
 // ... register commands
+$application->add(new printStrCommand('this str'));
 
 $application->run();
