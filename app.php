84bbfa7d1255eeb0242483bbc16a4e6f24c6fa7e
pashaapsky <ap.sky@yandex.ru>
2020-08-31 21:13:47 +0300

    add third symfony console command

--- app.php
+++ app.php
@@ -3,10 +3,12 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use App\Command\questCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
 // ... register commands
+$application->add(new questCommand());
 
 $application->run();
